3186267834dbf74cb967a87ca3643ce8afa38db7
Joerg Herrmann <jh_816@hotmail.com>
2022-01-04 00:41:04 +0100

    init

---
+++ software/PIUSV+/piusv-mqtt.pl
@@ -0,0 +1,29 @@
+package main;
+
+use 5.018;
+use feature qw( lexical_subs );
+
+use strict;
+use warnings;
+use utf8;
+use RPi::I2C;
+use EV;
+use AnyEvent;
+use AnyEvent::MQTT;
+
+my $piusv_device = RPi::I2C->new(0x18);
+
+my $i2c_timer = AnyEvent->timer (
+	after => 0,
+	interval => 1,
+	cb => sub {
+		# read status
+		my $status = $piusv_device->read_byte(0x00);
+		my @values = $piusv_device->read_bytes(10, 0x02);
+	},
+);
+
+my $quit = AnyEvent->condvar;
+$quit->recv();
+
+1;
