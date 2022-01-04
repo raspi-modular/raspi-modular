1d1ccc237b0dd9dab74d0aef91cdf76060d5ad21
Joerg Herrmann <jh_816@hotmail.com>
2022-01-04 01:26:58 +0100

    init

--- software/PIUSV+/piusv-mqtt.pl
+++ software/PIUSV+/piusv-mqtt.pl
@@ -20,10 +20,11 @@ my $i2c_timer = AnyEvent->timer (
 	interval => 1,
 	cb => sub {
 		# read status
-		my $status = $piusv_device->read_byte(0x00);
-		my @values = $piusv_device->read_bytes(10, 0x02);
+		#my $status = $piusv_device->read_byte(0x00);
+		$piusv_device->write(0x02);
+		my @values = $piusv_device->read_block(10);
 		say join ':', @values;
-		my ($battery, $current, $voltage, $voltage_usb, $voltage_external) = unpack ('n5', @values);
+		my ($battery, $current, $voltage, $voltage_usb, $voltage_external) = unpack ('n*', @values);
 		say $battery;
 		say $current;
 		say $voltage;
